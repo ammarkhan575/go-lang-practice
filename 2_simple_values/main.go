fcfe37d0e02f291391313a56db47693ff0e4f27e
Mohd Ammar <mohd.ammar@squareboat.com>
2026-02-10 12:58:06 +0530

    ADD: simple values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values
+	//integer
+	fmt.Println(1);
+	fmt.Println(1+1);
+
+	//boolean
+	fmt.Println(true);
+	fmt.Println(false);
+	fmt.Println(true && false);
+	fmt.Println(true || false);
+	fmt.Println(!true);
+
+	//string
+	fmt.Println("go" + "lang");
+
+	//float 
+	fmt.Println(10.33);
+	fmt.Println(1.0/3.0);
+}
