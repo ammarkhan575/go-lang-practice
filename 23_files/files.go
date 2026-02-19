77a362bb0086d3b743be83af2e1ddf0ce3eaf9f0
ammarkhan575 <ammarkhan575@gmail.com>
2026-02-19 23:25:52 +0530

    ADD: enhance file reading functionality and directory listing in files.go

--- 23_files/files.go
+++ 23_files/files.go
@@ -53,10 +53,42 @@ func main() {
 	buffer := make([]byte, example1Info.Size())
 
 	// read the content of the file and store it in the buffer
+	// _ is buffer length
 	_, err = example1.Read(buffer) // here we pass the buffer to the Read method and it will read the content of the file and store it in the buffer
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return
 	}
-	fmt.Println("file content:", buffer)
+	fmt.Println("file content:", string(buffer))
+	// we can also iterate over the buffer and print each byte and its corresponding character
+	for i:=0; i<len(buffer);i++ {
+		fmt.Println("byte:", buffer[i], "character:", string(buffer[i]))
+	}
+
+
+	// we can use the os.ReadFile function to read the content of the file in one line of code
+	// it is not recommended to use os.ReadFile for large files as it will read the entire file into memory and can cause out of memory error
+	content, err := os.ReadFile("23_files/example1.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("file content using os.ReadFile:", string(content))
+
+	dir, err := os.Open(".")
+	if err != nil {
+		fmt.Println("Error opening directory:", err)
+		return
+	}
+	defer dir.Close()
+	fileInfo2, err := dir.ReadDir(40) // read the content of the directory and store it in a slice of os.DirEntry
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+
+	for _, file := range fileInfo2 {
+		fmt.Println("file name:", file.Name())
+		fmt.Println("is directory:", file.IsDir())
+	}
+	
 }
